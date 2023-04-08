2c6fbf1dba715600e29c546ba656d0ab4dd36325
Bilal Yousef <s1169284@student.windesheim.nl>
2023-04-08 22:58:07 +0200

    added data to viewmodel

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -1,16 +1,20 @@
-﻿namespace AirBnbMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirBnbMVC.Models
 {
     public class Reservation
     {
 
         public int Id { get; set; }
+        [Display(Name = "Startdatum")]
         public DateTime StartDate { get; set; }
-
+        [Display(Name = "Einddatum")]
         public DateTime EndDate { get; set; }
 
         public int UserId { get; set; }
         public User User { get; set; }
         public int PropertyId { get; set; }
+        [Display(Name = "Locatie")]
         public Property Property { get; set; }
 
         public string StartDateAsString { get => StartDate.ToShortDateString(); }
--- Viewmodels/ReservationsViewmodel.cs
+++ Viewmodels/ReservationsViewmodel.cs
@@ -1,6 +1,24 @@
-﻿namespace AirBnbMVC.Viewmodels
+﻿using AirBnbMVC.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AirBnbMVC.Viewmodels
 {
     public class ReservationsViewmodel
     {
+        public int Id { get; set; }
+        [Display(Name = "Startdatum")]
+        public DateTime StartDate { get; set; }
+        [Display(Name = "Einddatum")]
+        public DateTime EndDate { get; set; }
+        [Display(Name = "Voornaam")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Achternaam")]
+        public string LastName { get; set; }
+
+        public int PropertyId { get; set; }
+        [Display(Name = "Locatie")]
+        public Property Property { get; set; }
     }
 }
