1f19ce8d08ed21795f86cf08cd8f814303d17a93
Bilal Yousef <s1169284@student.windesheim.nl>
2023-04-09 18:41:28 +0200

    seperated business logic from reservationscontroller

--- Controllers/ReservationsController.cs
+++ Controllers/ReservationsController.cs
@@ -20,78 +20,54 @@ namespace AirBnbMVC.Controllers
             _context = context;
         }
 
-        // GET: Reservations/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(int? id, ReservationsViewmodel viewModel)
         {
-            if (id == null || _context.Reservations == null)
-            {
-                return NotFound();
-            }
+            //if (id == null || _context.Reservations == null)
+            //{
+            //    return NotFound();
+            //}
 
-            var reservation = await _context.Reservations
-                .Include(r => r.Property)
-                .ThenInclude(p => p.Landlord)
-                .Include(r => r.User)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (reservation == null)
-            {
-                return NotFound();
-            }
-            var property = reservation.Property;
+            //var reservation = await _context.Reservations
+            //    .Include(r => r.Property)
+            //    .ThenInclude(p => p.Landlord)
+            //    .Include(r => r.User)
+            //    .FirstOrDefaultAsync(m => m.Id == id);
+            //if (reservation == null)
+            //{
+            //    return NotFound();
+            //}
+            //var property = reservation.Property;
             ReservationsViewmodel vm = new ReservationsViewmodel
             {
-                Reservation = reservation,
-                Property = property
+                Context = _context,
             };
+            await vm.GetDetails(id);
             return View(vm);
         }
 
-        // GET: Reservations/Create
+        
         [Route("Create/{propertyId}")]
         public IActionResult Create([FromRoute] int propertyId)
         {
-            var property = _context.Properties.FirstOrDefault(p => p.Id == propertyId);
-            
             var vm = new ReservationsViewmodel
             {
-                Property = property
+                Context = _context,
             };
-             
+            vm.Load(propertyId);
+
             return View(vm);
         }
 
-        // POST: Reservations/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [Route("Create/{propertyId}")]
         public async Task<IActionResult> Create(ReservationsViewmodel viewModel)
         {
-           
-                // Create a new User instance with the data from the UserViewModel
-                var newUser = new User
-                {
-                    FirstName = viewModel.FirstName,
-                    LastName = viewModel.LastName
-                };
+                
+            viewModel.Context = _context;
+            await viewModel.CreateReservation(viewModel);
 
-                // Add the new user to the database
-                _context.Users.Add(newUser);
-                await _context.SaveChangesAsync();
-
-                // Use the new user's Id to create the Reservation instance
-                var reservation = new Reservation
-                {
-                    StartDate = viewModel.StartDate,
-                    EndDate = viewModel.EndDate,
-                    UserId = newUser.Id,
-                    PropertyId = viewModel.PropertyId
-                };
-
-                _context.Reservations.Add(reservation);
-                await _context.SaveChangesAsync();
-
-                return RedirectToAction(nameof(Details), new { id = reservation.Id });
+            return RedirectToAction(nameof(Details), new { id = viewModel.Reservation.Id });
         }
 
     }
--- Viewmodels/ReservationsViewmodel.cs
+++ Viewmodels/ReservationsViewmodel.cs
@@ -1,4 +1,5 @@
 ﻿using AirBnbMVC.Models;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.Xml.Linq;
 
@@ -6,14 +7,18 @@ namespace AirBnbMVC.Viewmodels
 {
     public class ReservationsViewmodel
     {
+        public AirBnbContext Context { get; set; }
         public int Id { get; set; }
+        [Required]
         [Display(Name = "Startdatum")]
         public DateTime StartDate { get; set; }
+        [Required]
         [Display(Name = "Einddatum")]
         public DateTime EndDate { get; set; }
+        [Required]
         [Display(Name = "Voornaam")]
         public string FirstName { get; set; }
-
+        [Required]
         [Display(Name = "Achternaam")]
         public string LastName { get; set; }
 
@@ -37,6 +42,63 @@ namespace AirBnbMVC.Viewmodels
                 return subtotal;
             }
         }
+        public async Task CreateReservation(ReservationsViewmodel viewModel)
+        {
+            var reservations = Context.Reservations.Where(reservation => reservation.Property == Property && 
+            ((StartDate >= reservation.StartDate && EndDate <= reservation.EndDate)
+            || (StartDate >= reservation.StartDate && StartDate <= reservation.EndDate)));
+            if (reservations.Any())
+            {
+                throw new Exception("Er bestaat al een reservering op deze datum");
+            }
+            var newUser = new User
+            {
+                FirstName = FirstName,
+                LastName = LastName
+            };
 
+            Context.Users.Add(newUser);
+            await Context.SaveChangesAsync();
+
+
+            var reservation = new Reservation
+            {
+                StartDate = StartDate,
+                EndDate = EndDate,
+                UserId = newUser.Id,
+                PropertyId = PropertyId
+            };
+
+            Context.Reservations.Add(reservation);
+            await Context.SaveChangesAsync();
+
+            viewModel.Reservation = reservation;
+        }
+        public void Load(int propertyId)
+        {
+            Property = Context.Properties.FirstOrDefault(p => p.Id == propertyId);
+        }
+
+        public async Task GetDetails(int? id)
+        {
+            if (id == null || Context.Reservations == null)
+            {
+                throw new Exception("Geen reservering gevonden.");
+            }
+
+            var reservation = await Context.Reservations
+                .Include(r => r.Property)
+                .ThenInclude(p => p.Landlord)
+                .Include(r => r.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reservation == null)
+            {
+                throw new Exception("Geen reservering gevonden.");
+            }
+            var property = reservation.Property;
+
+            Property = property;
+            Reservation = reservation;
+        }
     }
 }
